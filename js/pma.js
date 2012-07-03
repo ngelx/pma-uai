238923813d5aa687d10d28817b441b4628416f82
Angel <angel.arancibia@gmail.com>
2012-07-03 12:07:49 -0300

    Added logic to check drag&drop

--- js/pma.js
+++ js/pma.js
@@ -1,16 +1,15 @@
 
 // Atach events to HTMLElements
-$(document).ready(function(){
-  // TODO: terminar bien esto, hayq ue agregar el resto de los autores
-  
+$(document).ready(function(){  
   // Baricco,shirky, tovalds, piscitelli, levy
   var autors = ["autor_1","autor_2","autor_3","autor_4","autor_5"]
 
   for(i in autors){
+    // addevents to autors
     var autor = document.getElementById(autors[i]);
     autor.addEventListener('dragstart', handleDragStart, false);
     
-    // TODO: terminar bien esto, hay que agregar el resto de los autores    
+    // addevents to frases
     var frases = document.getElementsByName(autors[i]);
     [].forEach.call(frases, function(frase) {
       frase.addEventListener('drop', handleDrop, false);
@@ -18,26 +17,38 @@ $(document).ready(function(){
   }
 })
 
-var dragSrcEl = null;
+// global variable that copntains the id dragged
+var dragData = null;
 
 function handleDragStart(e) {
   // Target (this) element is the source node.
   this.style.opacity = '0.4';
-  console.log("drag:" + this)
 
-  dragSrcEl = this;
+  dragData = this;
 
-  //e.dataTransfer.effectAllowed = 'move';
-  //e.dataTransfer.setData('text/html', this.innerHTML);
+  e.dataTransfer.effectAllowed = 'move';
 }
  
 function handleDrop(e) {
   // this/e.target is current target element.
-  console.log("drop:" + this)
   if (e.stopPropagation) {
     e.stopPropagation(); // Stops some browsers from redirecting.
   }
+  
+  if (dragData.id === this.getAttribute('name')) {
+    handleOk(this,dragData)
+  }
+  else {
+    handleWrong(this,dragData)
+  }
+  
   return false;
 } 
 
-  
+function handleOk(fraseElment, autor){
+  console.log("ok")
+}
+
+function handleWrong(fraseElment, autor){
+  console.log("mal")
+}
