622f237970e513a7ad69de953d1d086bf42d67e6
Angel <angel.arancibia@gmail.com>
2012-07-03 13:06:10 -0300

    another bugfix

--- js/pma.js
+++ js/pma.js
@@ -55,6 +55,12 @@ function checkResult(fraseElement, autorElement){
 function handleOk(fraseElement, autorElement){
   // TODO: hacer algo interesante ...
   // TODO: chequear si fue marcada como okay ya ...
+  if (fraseElement.className === "frase_okay") {
+    console.log("ya esta okay")
+    return
+  }
+  
+  fraseElement.className = "frase_ok"
   i = fraseElement.children[0].innerHTML
   autor = autorElement.children[0].innerHTML
   i = i + '<label class="autor_label"> - ' + autor + '</label>'
